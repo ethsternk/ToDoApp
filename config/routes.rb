601b1fd27505cb9866a57a94a148981a05812a10
Ethan Sternke <ethan.sternke@gmail.com>
2019-07-10 16:14:51 -0400

    create new todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'todos' => 'todos#index'
+  get 'todos' => 'todos#index', as: :home
+  get 'todos/new' => 'todos#new', as: :new
+  post 'todos' => 'todos#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
